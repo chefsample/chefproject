21b477a7b9edd8e7b8a1014f94734db32bd3d6f3
kumar <kumar0616@hotmail.com>
2017-08-17 22:50:10 +0000

    Updated default.rb file

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #
 
-if node['platform_family'] == "rhel"
+if node['platform_family'] == "amazon"
 	package = "httpd"
 elsif node['platform_family'] == "debian"
 	package = "apache2"
